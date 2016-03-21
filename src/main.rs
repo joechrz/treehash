0e77242a6dd23fe1d4f95d1111328ead36af6c9f
Joe C <joechrz@gmail.com>
2016-03-20 22:35:06 -0700

    docs

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,15 @@ use docopt::Docopt;
 mod tree_hash;
 
 const USAGE: &'static str = "
-Usage: treehash [options] <filename>
-       treehash (--help | --version)
+treehash 
+
+calculates the sha256 tree-hash according to the algorithm laid out at
+http://docs.aws.amazon.com/amazonglacier/latest/dev/checksum-calculations.html
+
+if no filename is specified, reads from <stdin>
+
+Usage: treehash [options] [<filename>]
+       treehash --help
 
 Options:
   -b, --binary      Output the result in binary form (default: hex string)
